220e299e2822d15ad20458592b7d1af1a4c99ee2
Julia Grimes <julia.s.grimes@gmail.com>
2022-12-26 21:02:03 -0500

    Add experience model

---
+++ db/migrate/20221227020115_create_experiences.rb
@@ -0,0 +1,14 @@
+class CreateExperiences < ActiveRecord::Migration[7.0]
+  def change
+    create_table :experiences do |t|
+      t.date :start_date
+      t.date :end_date
+      t.string :job_title
+      t.string :company_name
+      t.text :details
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_015030) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_020115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_015030) do
     t.string "degree"
     t.string "university_name"
     t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "job_title"
+    t.string "company_name"
+    t.text "details"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
