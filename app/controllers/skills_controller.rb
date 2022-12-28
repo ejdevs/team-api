fd6489b9c9f259bae0d574cd8ef9cf024b6dbdb6
Julia Grimes <julia.s.grimes@gmail.com>
2022-12-27 19:48:11 -0500

    Added routes for all models CRUD

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,3 @@
 class SkillsController < ApplicationController
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,41 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  post "/students" => "students#create"
+  #LOGIN
   post "/sessions" => "sessions#create"
+
+  #STUDENTS CRUD
+  post "/students" => "students#create"
+  get "/students" => "students#index"
+  get "/students/:id" => "students#show"
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#destroy"
+
+  #SKILLS CRUD
+  get "/skills" => "skills#index"
+  post "/skills" => "skills#create"
+  get "/skills/:id" => "skills#show"
+  patch "/skills/:id" => "skills#update"
+  delete "/skills/:id" => "skills#destroy"
+
+  #EXPERIENCES CRUD
+  get "/experiences" => "experiences#index"
+  post "/experiences" => "experiences#create"
+  get "/experiences/:id" => "experiences#show"
+  patch "/experiences/:id" => "experiences#update"
+  delete "/experiences/:id" => "experiences#destroy"
+
+  #CAPSTONES CRUD
+  get "/capstones" => "capstones#index"
+  post "/capstones" => "capstones#create"
+  get "/capstones/:id" => "capstones#show"
+  patch "/capstones/:id" => "capstones#update"
+  delete "/capstones/:id" => "capstones#destroy"
+
+  #EDUCATIONS CRUD
+  get "/educations" => "educations#index"
+  post "/educations" => "educations#create"
+  get "/educations/:id" => "educations#show"
+  patch "/educations/:id" => "educations#update"
+  delete "/educations/:id" => "educations#destroy"
 end
