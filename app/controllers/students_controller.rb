66277203c8555421e01a0440b71f58e87b863004
Evan <evanjdevelopment@gmail.com>
2022-12-27 19:52:05 -0600

    finalized student controller crud actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -24,4 +24,32 @@ class StudentsController < ApplicationController
       render json: { errors: student.errors.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    student = Student.find_by(id: params[:id])
+    student.update(
+      first_name: params[:name] || student.first_name,
+      last_name: params[:last_name] || student.last_name,
+      email: params[:email] || student.email,
+      phone_number: params[:phone_number] || student.phone_number,
+      short_bio: params[:short_bio] || student.short_bio,
+      linkedin_url: params[:linkedin_url] || student,linkedin_url,
+      twitter_handle: params[:twitter_handle] || student.twitter_handle,
+      personal_site_url: params[:personal_site_url] || student.personal_site_url,
+      online_resume_url: params[:online_resume_url] || student.online_resume_url,
+      github_url: params[:github_url] || student.github_url,
+      photo_url: params[:photo_url] || student.photo_url,
+    )
+    if student.save
+    render json: student.as_json
+  end
+end
+
+  def destroy
+    id = params[:id]
+    student = Student.find(:id)
+
+    student.destroy
+    render json: { message: "This student has been deleted." }
+  end
 end
