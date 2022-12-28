847a38fe99b4df44cb7504b07cd0dcfbde05a2d1
Evan <evanjdevelopment@gmail.com>
2022-12-27 18:45:10 -0600

    moved to new branch

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   post "/students" => "students#create"
+  get "/students" => "students#index"
+  get "/students/:id" => "students#show"
 end
