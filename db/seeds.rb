13e6c2dd96832ad4d88617cf21a4091316b509fb
Evan <evanjdevelopment@gmail.com>
2022-12-28 19:27:25 -0600

    lazy commit message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Student.create!(
+  first_name: "First",
+  last_name: "Last Name",
+  email: "test@person.com",
+  phone_number: 3526,
+  short_bio: "I'm a person",
+  linkedin_url: "https://www.linkedin.com/in/adam-folta/",
+  twitter_handle: "twitter",
+  personal_site_url: "site",
+  online_resume_url: "https://via.placeholder.com/150",
+  github_url: "afolta",
+  photo_url: "photo",
+  password: "password",
+)
